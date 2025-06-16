7ee3d0afed8a93d00ac70966a485b8c7c49e1617
Anisha-khadka <anishakhadka434@gmail.com>
2025-06-16 22:34:14 +0545

    third commit

--- task2.py
+++ task2.py
@@ -3,34 +3,45 @@
 # while asking for number, check whether user has sent a number or not
 # If user has sent number then do the calculation
 # else ask the user for number again and again whether its a number
+# new requirement
+
+#do exception handling and handle the int conversion error in such a way that if the error occurs ask the user for the number again but if the error doesnot occur then continue the calculator
 
 
 while True:
-    first_input = input("Enter first number:")
-    if first_input.isdigit():
-        first_input = int(first_input)
-    else:
-        print("Please enter valid number")
+    
+    try :
+        first_input = int(input("Enter first number:"))
+    except ValueError:
         continue
-
-    second_input = input("Enter second number:")
-    if second_input.isdigit():
-        second_input = int(second_input)
-    else:
-        print("Please enter valid number")
+    # if first_input.isdigit():
+    #     first_input = int(first_input)
+    # else:
+    #     print("Please enter valid number")
+    #     continue
+    try:
+        second_input = int(input("Enter second number:"))
+    except ValueError:
         continue
-
-    operator_input = input("Enter the operation to perform(+,-,*,/,%):")
-    if operator_input == '+':
-        print(first_input+second_input)
-    elif operator_input == '-':
-        print(first_input-second_input)
-    elif operator_input == '*':
-        print(first_input*second_input)
-    elif operator_input == '/':
-        print(first_input/second_input)
-    elif operator_input == '%':
-        print(first_input % second_input)
-    else:
-        print("Please enter the correct operator")
+    # if second_input.isdigit():
+    #     second_input = int(second_input)
+    # else:
+    #     print("Please enter valid number")
+    #     continue
+    try:
+        operator_input = input("Enter the operation to perform(+,-,*,/,%):")
+        if operator_input == '+':
+            print(first_input+second_input)
+        elif operator_input == '-':
+            print(first_input-second_input)
+        elif operator_input == '*':
+            print(first_input*second_input)
+        elif operator_input == '/':
+            print(first_input/second_input)
+        elif operator_input == '%':
+            print(first_input % second_input)
+    except:
         continue
+    # else:
+    #     print("Please enter the correct operator")
+    #     continue
